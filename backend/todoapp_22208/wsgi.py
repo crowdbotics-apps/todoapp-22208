8ea72e47b02cf85e62b02b31a8a8ad37badc3d43
Crowdbotics <team@crowdbotics.com>
2020-11-02 10:40:53 +0000

    Update models

--- backend/todoapp_22208/wsgi.py
+++ backend/todoapp_22208/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoapp_22208.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoapp_22208.settings")
 
 application = get_wsgi_application()
